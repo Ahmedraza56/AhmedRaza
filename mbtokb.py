57bf18064df9412d56d528589e518363bd551f74
ahmed <ahmedrazy830@gmail.com>
2023-12-18 10:54:08 +0500

    initial commit

--- mbtokb.py
+++ mbtokb.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, send_file ,Blueprint
+from flask import Blueprint, render_template, request, send_file, url_for
 from PIL import Image
 import os
 
-mbtokb_app = Blueprint("mbtokb",__name__)
+mbtokb_app = Blueprint("mbtokb", __name__)
 
 def mb_to_kb_converter(image_path, output_path, target_size_kb):
     try:
@@ -16,20 +16,24 @@ def mb_to_kb_converter(image_path, output_path, target_size_kb):
                 img.save(output_path, format="JPEG", quality=85)
 
     except Exception as e:
+        print(f"Error during image conversion: {e}")
         return None
 
     return output_path
 
+def get_static_path(filename):
+    return os.path.join("static", filename)
+
 @mbtokb_app.route('/', methods=['GET', 'POST'])
 def index():
-    download_button_visible = False  # Flag to determine if the "Download" button should be visible
+    download_button_visible = False
 
     if request.method == 'POST':
-        uploaded_file = request.files['file']
+        uploaded_file = request.files.get('file')
         if uploaded_file:
             target_size_kb = 100
-            output_path = "static/converted_image.jpg"
-            uploaded_file_path = f"static/{uploaded_file.filename}"
+            output_path = get_static_path("converted_image.jpg")
+            uploaded_file_path = get_static_path(uploaded_file.filename)
 
             uploaded_file.save(uploaded_file_path)
 
@@ -37,13 +41,12 @@ def index():
 
             if converted_path:
                 download_button_visible = True
-                return render_template('mbtokb.html', original_image=uploaded_file_path,
-                                       converted_image=converted_path,
+                return render_template('mbtokb.html', original_image=url_for('static', filename=uploaded_file.filename),
+                                       converted_image=url_for('static', filename='converted_image.jpg'),
                                        download_button_visible=download_button_visible)
 
     return render_template('mbtokb.html', download_button_visible=download_button_visible)
 
 @mbtokb_app.route('/download')
 def download():
-    return send_file('static/converted_image.jpg', as_attachment=True)
-
+    return send_file(get_static_path("converted_image.jpg"), as_attachment=True)
